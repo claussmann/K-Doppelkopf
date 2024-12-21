d289221499ea911daa0bf30c53cc083e0a451405
christian <c@laussmann.me>
2024-12-21 16:11:19 +0100

    INIT: Ktor application

---
+++ src/main/kotlin/doppelkopf/Routing.kt
@@ -0,0 +1,19 @@
+package doppelkopf
+
+import com.fasterxml.jackson.databind.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        staticResources("/static", "static")
+    }
+}
