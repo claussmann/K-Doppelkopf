9c3bdda8643dd6943303a7f90c591cb019afc639
christian <c@laussmann.me>
2024-12-21 20:15:26 +0100

    REFACTOR: Umbenennen in Kartenstapel

--- src/main/kotlin/doppelkopf/game/Game.kt
+++ src/main/kotlin/doppelkopf/game/Game.kt
@@ -1,16 +1,15 @@
 package doppelkopf.game
 
 class Game(val spieler: Array<Spieler>) {
-    val tisch: Tisch = Tisch()
+    val karten = Kartenstapel()
 
     init {
         if (spieler.size != 4) throw IllegalArgumentException("Spieler must have 4 spielers")
     }
 
     fun neuGeben() {
-        tisch.mischen()
         for (s in spieler) {
-            s.neueHand(tisch.zieheKarten())
+            s.neueHand(karten.zieheKarten())
         }
     }
 }
