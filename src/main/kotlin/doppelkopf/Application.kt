b66270c884c8d4580d16abe44990d83e9af2f3ed
christian <c@laussmann.me>
2025-01-18 14:50:22 +0100

    ADD: Endpoint for joining

--- src/main/kotlin/doppelkopf/Application.kt
+++ src/main/kotlin/doppelkopf/Application.kt
@@ -1,6 +1,9 @@
 package doppelkopf
 
+import doppelkopf.service.DoppelkopfService
 import io.ktor.server.application.*
+
+val service = DoppelkopfService()
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
