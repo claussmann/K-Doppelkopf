53617f671fec3ae10bb9158aef138421381fe3cc
christian <c@laussmann.me>
2025-02-08 19:52:43 +0100

    REFACTOR: Configuration in code

--- src/main/kotlin/doppelkopf/Application.kt
+++ src/main/kotlin/doppelkopf/Application.kt
@@ -2,11 +2,14 @@ package doppelkopf
 
 import doppelkopf.service.DoppelkopfService
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 val service = DoppelkopfService()
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
